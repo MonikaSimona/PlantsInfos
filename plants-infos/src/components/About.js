e0811c65117ff1a963fb5e7a3480ea66c6c65544
Simona Simonovska <56088273+MonikaSimona@users.noreply.github.com>
2021-02-03 14:33:33 +0100

    final

--- plants-infos/src/components/About.js
+++ plants-infos/src/components/About.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import  {RiPlantFill} from 'react-icons/ri';
 const About = () => {
     return ( 
-        <div className="container">
-            <h4 className="center">About PAGE</h4>
-            <p className="center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, sed!</p>
+        <div className="container about">
+            <h2 className="center mt-4 mb-3">The Gardener </h2>
+            <p className="center">Hi everyone! I am Simona, and as you guessed i love plants. Here in my online garden I arranged some infos of my favourite plants. <br/> 
+            I like the color green, and yeey all the plants have green part. Also the color violet is on the top of my color list, so you can spot some violet themed flowers in my garden.Plants make me think of growth, and that is important think in our lifes. They never stop growing. We can learn a valuable lesson, so my friends never give up and keep growing. 
+            <br/> <RiPlantFill/>
+            </p>
         </div>
      );
 }
--- plants-infos/src/components/Contact.js
+++ plants-infos/src/components/Contact.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import  { FaFacebookMessenger, FaPinterest } from 'react-icons/fa';
+import { AiFillInstagram, AiFillBehanceCircle } from 'react-icons/ai';
 const Contact = (props) => {
-    setTimeout(() => {
-        props.history.push('/about');
-    }, 2000);
+    // setTimeout(() => {
+    //     props.history.push('/about');
+    // }, 2000);
     return ( 
-        <div className="container">
-            <h4 className="center">Contact PAGE</h4>
-            <p className="center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, sed!</p>
+        <div className="container contact ">
+            <h2 className="center mt-4 mb-3">Where you can find the Gardener</h2>
+            <p className="center">You can find me on this social links and we can chat about plants.</p>
+            <br/>
+            <div className="icons">
+            <a href="https://www.facebook.com/simona.simonovska.5895/" target="_blank" rel="noreferrer" >
+            <FaFacebookMessenger className="icon"/> </a>
+            <a href="https://www.instagram.com/si.monkaa/" target="_blank" rel="noreferrer">
+            <AiFillInstagram className="icon"/></a>
+            <a href="https://www.behance.net/simonasimonov/projects" target="_blank" rel="noreferrer">
+            <AiFillBehanceCircle className="icon"/> </a>
+            
+            <a href="https://www.pinterest.com/monisimona98/_saved/" target="_blank" rel="noreferrer">
+            <FaPinterest className="icon"/> </a>
+            </div>
         </div>
      );
 }
